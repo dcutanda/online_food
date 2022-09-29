054421016a43e4ca1c3f9754261dc87e3897fad5
drax <programpractice21@gmail.com>
2022-09-29 20:15:19 +0800

    Create cart model, add_to_cart url, sending ajax request, add_to_cart view functionality

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,9 +1,12 @@
 from unicodedata import category
 from django.shortcuts import render, get_object_or_404
+from marketplace.models import Cart
 from vendor.models import Vendor
 from menu.models import Category, FoodItem
 
 from django.db.models import Prefetch
+
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 
@@ -31,3 +34,25 @@ def vendor_detail(request, vendor_slug):
         'categories': categories,
     }
     return render(request, 'marketplace/vendor_detail.html', context)
+
+def add_to_cart(request, food_id):
+    if request.user.is_authenticated:
+        if request.is_ajax():
+            # Check if food exist in marketplace
+            try:
+                fooditem = FoodItem.objects.get(id=food_id)
+                # Check if already added to the cart
+                try:
+                    chkCart = Cart.objects.get(user=request.user, fooditem=fooditem)
+                    chkCart.quantity += 1
+                    chkCart.save()
+                    return JsonResponse({'status': 'Success', 'messages': 'Increased the cart quantity!'})
+                except:
+                    chkCart = Cart.objects.create(user=request.user, fooditem=fooditem, quantity=1)
+                    return JsonResponse({'status': 'Success', 'messages': 'Added the food to the cart'})
+            except:
+                return JsonResponse({'status': 'Failed', 'messages': 'This food does not exist'})
+        else:
+            return JsonResponse({'status': 'Failed', 'messages': 'Invalid Request'})
+    else:
+        return JsonResponse({'status': 'Failed', 'messages': 'Please log in to continue!'})
